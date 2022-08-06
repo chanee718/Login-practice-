c9524be8e11e14dcf3b41541eaead90a23c01a97
chanee718 <chanee718@kaist.ac.kr>
2022-08-06 22:28:07 +0900

    using DOM

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,3 +1,17 @@
 "use strict";
 
-console.log("hello");
+//DOM: document object model (일종의 인터페이스) - JS에서 html 에 존재하는 데이터를 가져와서 사용!
+
+const id = document.querySelector("#id"),
+    pwd = document.querySelector("#pwd"),
+    loginbutton = document.querySelector("button");
+
+loginbutton.addEventListener("click", login);
+
+function login() {
+    const req = {
+        id: id.value,
+        pwd: pwd.value,
+    };
+    console.log(req);
+}
